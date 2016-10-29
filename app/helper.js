c86bd712b53961b180dbcbc15ec3bda149215ebd
Matt D <matt.davenport@gmail.com>
2016-10-29 16:32:39 -0400

    WIP: Adding config validation

--- app/helper.js
+++ app/helper.js
@@ -5,7 +5,7 @@ const helpers = {};
 
 /**
  * Verify config:
- *  - GitHub/Trello credentials exist
+ *  - GitHub/Trello auth credentials exist
  *  - No conflicting auth method/credential pairs
  *  - Proper mappings are in place for (n+1) sync jobs
  *
@@ -14,7 +14,42 @@ const helpers = {};
  * @returns {string}
 */
 helpers.validateConfig = function validateConfig(config) {
+  // check credentials
+  let githubAuthMethod = config.get('github:config:authMethod');
+  let trelloAuthMethod = config.get('trello:config:authMethod');
 
-};
+  if (_.isUndefined(githubAuthMethod)) throw "Undefined authentication method [GitHub]";
+  if (_.isUndefined(trelloAuthMethod)) throw "Undefined authentication method [Trello]";
+
+  var validateGithub = function (type) {
+    switch (type) {
+      case 'basic':
+        return !(_.isUndefined(config.get('github:auth:basic:username') ||
+                _.isUndefined(config.get('github:auth:basic:password'))));
+      case 'oauth':
+        return !(_.isUndefined(config.get('github:auth:oauth:key')) ||
+                _.isUndefined(config.get('github:auth:oauth:secret')));
+      case 'token':
+        return !(_.isUndefined(config.get('github:auth:token:token')));
+      default:
+        throw "Invalid authentication method [GitHub]";
+    }
+  }.bind(config);
+
+  var validateTrello = function (type) {
+    switch (type) {
+      case 'basic':
+        return !(_.isUndefined(config.get('trello:auth:key')) ||
+                _.isUndefined(config.get('trello:auth:token')));
+      default:
+        throw "Invalid authentication method [Trello]";
+    }
+  }.bind(config);
+
+  if (!validateGithub(githubAuthMethod) || !validateTrello(trelloAuthMethod)) {
+    throw "Invalid authentication config. Please verify you have filled out all values for your selected auth method in config.json";
+  }
+
+  // check mappings
 
 exports.helpers = helpers;
