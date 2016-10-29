8b7e2693c63873063b7f06ad4be07d967aeaf4ff
Matt D <matt.davenport@gmail.com>
2016-10-29 15:34:23 -0400

    WIP: Adding eslint file. Update gitignore. Add helper skeleton. Fix some ESLint errors. Add nconf for config migration

---
+++ app/helper.js
@@ -0,0 +1,20 @@
+/**
+ * @summary   Helper functions for the node app
+ */
+const helpers = {};
+
+/**
+ * Verify config:
+ *  - GitHub/Trello credentials exist
+ *  - No conflicting auth method/credential pairs
+ *  - Proper mappings are in place for (n+1) sync jobs
+ *
+ * @param {string} str The string to repeat.
+ * @param {number} [times=1] How many times to repeat the string.
+ * @returns {string}
+*/
+helpers.validateConfig = function validateConfig(config) {
+
+};
+
+exports.helpers = helpers;
